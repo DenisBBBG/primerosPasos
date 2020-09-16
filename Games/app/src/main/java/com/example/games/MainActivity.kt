746eaeef3e123526bb1dd889aa4beb3237c19279
Denis <denis.bogomilov@babel.es>
2020-09-16 18:03:36 +0200

    Guardado de control

--- Games/app/src/main/java/com/example/games/MainActivity.kt
+++ Games/app/src/main/java/com/example/games/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.games
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewManager = LinearLayoutManager(this)
+
+
     }
 }
