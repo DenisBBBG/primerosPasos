f663f90631c8de94f8bf0e791a4d643c8c582df0
Denis <denis.bogomilov@babel.es>
2020-10-08 15:49:42 +0200

    3 - To correct the total games dialog provider
    Why:
              -The class was wrong
    How:
              -To correct the override fun

--- EMAprivado/app/src/main/java/es/babel/easymvvm/presentation/dialog/totalGames/TotalGamesDialogProvider.kt
+++ EMAprivado/app/src/main/java/es/babel/easymvvm/presentation/dialog/totalGames/TotalGamesDialogProvider.kt
@@ -3,8 +3,7 @@ package es.babel.easymvvm.presentation.dialog.totalGames
 import androidx.fragment.app.FragmentManager
 import es.babel.easymvvm.android.ui.dialog.EmaBaseDialog
 import es.babel.easymvvm.android.ui.dialog.EmaBaseDialogProvider
-import es.babel.easymvvm.presentation.dialog.simple.SimpleDialog
 
 class TotalGamesDialogProvider constructor(fragmentManager: FragmentManager) : EmaBaseDialogProvider(fragmentManager) {
-    override fun generateDialog(): EmaBaseDialog<*> = SimpleDialog()
+    override fun generateDialog(): EmaBaseDialog<*> = TotalGamesDialog()
 }
