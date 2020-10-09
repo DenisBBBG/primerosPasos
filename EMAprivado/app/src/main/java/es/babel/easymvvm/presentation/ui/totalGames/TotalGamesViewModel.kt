52b5f8016b3a45544e3bc9ff4f6f382da1079b18
Denis <denis.bogomilov@babel.es>
2020-10-09 13:19:44 +0200

    4 - Build necessary functions
    Why:
             -Need to have specific functions to recieve the information
    How:
             -Add the functions to the fragment and viewModel

--- EMAprivado/app/src/main/java/es/babel/easymvvm/presentation/ui/totalGames/TotalGamesViewModel.kt
+++ EMAprivado/app/src/main/java/es/babel/easymvvm/presentation/ui/totalGames/TotalGamesViewModel.kt
@@ -1,11 +1,29 @@
 package es.babel.easymvvm.presentation.ui.totalGames
 
+import es.babel.domain.usecase.GetTotalGamesUseCase
+import es.babel.easymvvm.core.state.EmaExtraData
 import es.babel.easymvvm.presentation.base.BaseViewModel
 
-class TotalGamesViewModel() : BaseViewModel<TotalGamesState, TotalGamesNavigator.Navigation>() {
+class TotalGamesViewModel(private val getTotalGamesUseCase: GetTotalGamesUseCase) : BaseViewModel<TotalGamesState, TotalGamesNavigator.Navigation>() {
     override val initialViewState: TotalGamesState = TotalGamesState()
 
+    companion object {
+        const val TOTAL_GAMES_DIALOG = 1
+    }
+
     override fun onStartFirstTime(statePreloaded: Boolean) {
+
+        executeUseCaseWithException({
+            val totalGames = getTotalGamesUseCase.execute(Unit)
+
+            updateToNormalState {
+                copy(
+                        totalGames = totalGames.count.toString()
+                )
+            }
+        }, { error ->
+            updateToErrorState(error)
+        })
     }
 
     fun onCancelDialogTotalGames() {
@@ -13,6 +31,13 @@ class TotalGamesViewModel() : BaseViewModel<TotalGamesState, TotalGamesNavigator
     }
 
     fun onConfirmDialogTotalGames() {
+        updateToNormalState()
         navigateBack()
+    }
+
+    fun onTotalGamesDialogShow() {
+        checkDataState {
+            updateToAlternativeState(EmaExtraData(TOTAL_GAMES_DIALOG,it.totalGames))
+        }
     }
 }
